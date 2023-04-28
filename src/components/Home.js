d9796aca21c33f7798a57e6787879970a8c8c048
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-04-28 21:57:20 +0300

    lesson

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,5 +10,15 @@ function Home({ isLoggedIn }) {
     </div>
   );
 }
+// function Home({ isSignedIn }) {
+//   // if the user isn't signed in, redirect them to the login page
+//   if (!isSignedIn) return <Redirect to="/login" />;
 
+//   // otherwise, return the home page
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// }
 export default Home;
